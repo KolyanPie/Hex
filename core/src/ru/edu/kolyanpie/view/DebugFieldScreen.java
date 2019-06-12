d45e6a7b4692420989e1bf53487cfe42f855a970
Michael <43596340+KolyanPie@users.noreply.github.com>
2019-06-12 16:49:25 +0300

    Update DebugFieldScreen for Field

--- core/src/ru/edu/kolyanpie/view/DebugFieldScreen.java
+++ core/src/ru/edu/kolyanpie/view/DebugFieldScreen.java
@@ -3,30 +3,14 @@ package ru.edu.kolyanpie.view;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.utils.viewport.FitViewport;
 
-import ru.edu.kolyanpie.model.Cell;
+import ru.edu.kolyanpie.model.Field;
 
 public class DebugFieldScreen implements Screen {
-    private Stage stage = new Stage(new FitViewport(1080, 1920));
+    private Field stage = new Field();
 
     @Override
     public void show() {
-        Cell cell;
-        for (int j = 0; j < 28; j++) {
-            cell = new Cell();
-            cell.setBounds(-20, 34 + j * 68, 38, 41);
-            stage.addActor(cell);
-            for (int i = 0; i < 26; i++) {
-                cell = new Cell();
-                cell.setBounds(i * 40, j * 68, 38, 41);
-                stage.addActor(cell);
-                cell = new Cell();
-                cell.setBounds(20 + i * 40, 34 + j * 68, 38, 41);
-                stage.addActor(cell);
-            }
-        }
         Gdx.input.setInputProcessor(stage);
     }
 
@@ -40,7 +24,7 @@ public class DebugFieldScreen implements Screen {
 
     @Override
     public void resize(int width, int height) {
-        stage.getViewport().update(width, height);
+        stage.resize(width, height);
     }
 
     @Override
