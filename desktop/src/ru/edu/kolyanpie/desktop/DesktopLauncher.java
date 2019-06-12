2292122dc5ab5db035c2657a1d75b425bb88fc7c
Michael <43596340+KolyanPie@users.noreply.github.com>
2019-06-12 10:59:18 +0300

    Add width, height desktop launch configuration

--- desktop/src/ru/edu/kolyanpie/desktop/DesktopLauncher.java
+++ desktop/src/ru/edu/kolyanpie/desktop/DesktopLauncher.java
@@ -9,6 +9,8 @@ public class DesktopLauncher {
     public static void main(String[] arg) {
         System.setProperty("user.name", "name");
         LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        config.width = 1080;
+        config.height = 1920;
         new LwjglApplication(new Hex(), config);
     }
 }
