f805b154cb73fa4400a5078f74f18a2bcb1ded27
Michael <43596340+KolyanPie@users.noreply.github.com>
2019-06-12 10:49:36 +0300

    Add Cell

---
+++ core/src/ru/edu/kolyanpie/model/Cell.java
@@ -0,0 +1,134 @@
+package ru.edu.kolyanpie.model;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.Touchable;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+
+public class Cell extends Actor {
+    private State state;
+    private Sprite sprite = new Sprite(new Texture(Gdx.files.internal("cell.png")));
+
+    public Cell() {
+        initialize();
+    }
+
+    public Cell(Color empty, Color red, Color blue) {
+        initialize();
+        State.EMPTY.setColor(empty);
+        State.RED.setColor(red);
+        State.BLUE.setColor(blue);
+    }
+
+    public void setRedColor(Color color) {
+        State.RED.setColor(color);
+    }
+
+    public void setBlueColor(Color color) {
+        State.BLUE.setColor(color);
+    }
+
+    public void setEmptyColor(Color color) {
+        State.EMPTY.setColor(color);
+    }
+
+    public Color getRedColor() {
+        return State.RED.getColor();
+    }
+
+    public Color getBlueColor() {
+        return State.BLUE.getColor();
+    }
+
+    public Color getEmptyColor() {
+        return State.EMPTY.getColor();
+    }
+
+    public boolean setRed() {
+        if (isEmpty()) {
+            state = State.RED;
+        }
+        return false;
+    }
+
+    public boolean setBlue() {
+        if (isEmpty()) {
+            state = State.BLUE;
+        }
+        return false;
+    }
+
+    @Override
+    public void draw(Batch batch, float parentAlpha) {
+        sprite.setSize(getWidth(), getHeight());
+        sprite.setPosition(getX(), getY());
+        sprite.setColor(state.getColor());
+        sprite.draw(batch);
+    }
+
+    @Override
+    public Actor hit(float x, float y, boolean touchable) {
+        if (x >= 0
+                && x <= getWidth() - 1
+                && y > 9 * getHeight() / 41
+                && y <= 31 * getHeight() / 41
+                ) {
+            return this;
+        }
+        for (int i = 0; i < 9; i++) {
+            if (x > (17 - 2 * i) * getWidth() / 41
+                    && x <= (21 + 2 * i) * getWidth() / 41
+                    && (y > i * getHeight() / 41
+                    && y <= (1 + i) * getHeight() / 41
+                    || y <= getHeight() - 1 - i
+                    && y > getHeight() - 1 - (i + 1))
+                    ) {
+                return this;
+            }
+        }
+        return null;
+    }
+
+    private boolean isEmpty() {
+        return state.equals(State.EMPTY);
+    }
+
+    private void initialize() {
+        state = State.EMPTY;
+        setTouchable(Touchable.enabled);
+        addListener(new ClickListener() {
+            public void clicked(InputEvent event, float x, float y) {
+                System.out.println(1);
+                if (!isEmpty()) {
+                    return;
+                }
+                //TODO: some action
+            }
+        });
+    }
+
+    private enum State {
+        EMPTY(new Color(1, 1, 1, 1)),
+        RED(new Color(1, 0, 0, 1)),
+        BLUE(new Color(0, 0, 1, 1));
+
+        private Color color;
+
+        private Color getColor() {
+            return color;
+        }
+
+        private void setColor(Color color) {
+            this.color = color;
+        }
+
+        State(Color color) {
+            this.color = color;
+        }
+    }
+}
