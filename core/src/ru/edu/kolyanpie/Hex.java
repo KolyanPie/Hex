f3037351270ba1667dbcd6e3d7cfa6ae4c9bf5d3
Michael <43596340+KolyanPie@users.noreply.github.com>
2019-06-18 18:57:28 +0300

    update Hex

--- core/src/ru/edu/kolyanpie/Hex.java
+++ core/src/ru/edu/kolyanpie/Hex.java
@@ -1,11 +1,26 @@
 package ru.edu.kolyanpie;
 
+import com.badlogic.gdx.Application;
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+
+import ru.edu.kolyanpie.view.MenuScreen;
 
 public class Hex extends Game {
 
     @Override
     public void create() {
+        if (Gdx.app.getType().equals(Application.ApplicationType.Android)) {
+        Gdx.input.setCatchBackKey(true);
+        Gdx.input.setCatchMenuKey(true);
+        }
+        setScreen(new MenuScreen(this));
+    }
 
+    public void changeScreen(Screen screen) {
+        Screen tmp = getScreen();
+        setScreen(screen);
+        tmp.dispose();
     }
 }
