eba3750bc1bbac38bf99bb586442a25371658cf5
Michael <43596340+KolyanPie@users.noreply.github.com>
2019-06-20 13:49:57 +0300

    update LocalGameScreen

--- core/src/ru/edu/kolyanpie/view/LocalGameScreen.java
+++ core/src/ru/edu/kolyanpie/view/LocalGameScreen.java
@@ -1,15 +1,22 @@
 package ru.edu.kolyanpie.view;
 
+import com.badlogic.gdx.Application;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import ru.edu.kolyanpie.Hex;
+import ru.edu.kolyanpie.controller.WinChecker;
 import ru.edu.kolyanpie.model.Field;
+import ru.edu.kolyanpie.model.State;
 
 public class LocalGameScreen implements Screen {
     private final Hex hex;
     private Field stage;
+    private List<List<State>> cells;
     private boolean blueTurn;
 
     public LocalGameScreen(Hex hex) {
@@ -21,6 +28,13 @@ public class LocalGameScreen implements Screen {
         stage = new Field(this::cellClicked);
         Gdx.input.setInputProcessor(stage);
         blueTurn = true;
+        cells = new ArrayList<>(11);
+        for (int i = 0; i < 11; i++) {
+            cells.add(new ArrayList<>(11));
+            for (int j = 0; j < 11; j++) {
+                cells.get(i).add(State.EMPTY);
+            }
+        }
     }
 
     @Override
@@ -38,17 +52,24 @@ public class LocalGameScreen implements Screen {
 
     @Override
     public void pause() {
-
+        if (Gdx.app.getType().equals(Application.ApplicationType.Android)) {
+            Gdx.input.setInputProcessor(null);
+            stage.dispose();
+        }
     }
 
     @Override
     public void resume() {
-
+        if (Gdx.app.getType().equals(Application.ApplicationType.Android)) {
+            stage = new Field(this::cellClicked, cells);
+            Gdx.input.setInputProcessor(stage);
+        }
+        stage.resize(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
     }
 
     @Override
     public void hide() {
-
+        Gdx.app.exit();
     }
 
     @Override
@@ -61,10 +82,12 @@ public class LocalGameScreen implements Screen {
             if (!stage.setBlue(i, j)) {
                 return;
             }
+            cells.get(i).set(j, State.BLUE);
         } else {
             if (!stage.setRed(i, j)) {
                 return;
             }
+            cells.get(i).set(j, State.RED);
         }
         blueTurn = !blueTurn;
     }
