2e88bd41379f30ab54c9856460e2e3db81ac7087
kolyanpie <k0lyan.pie@gmail.com>
2020-04-17 23:48:34 +0300

    update gradle

--- desktop/src/ru/edu/kolyanpie/desktop/DesktopLauncher.java
+++ desktop/src/ru/edu/kolyanpie/desktop/DesktopLauncher.java
@@ -1,14 +1,13 @@
 package ru.edu.kolyanpie.desktop;
 
-import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
-import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import ru.edu.kolyanpie.Hex;
 
 public class DesktopLauncher {
     public static void main(String[] arg) {
         System.setProperty("user.name", "name");
-        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-        new LwjglApplication(new Hex(), config);
+        Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+        new Lwjgl3Application(new Hex(), config);
     }
 }
