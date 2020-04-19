7f2f0437caf8248aaca8788eb1a1fb7209ba9380
kolyanpie <k0lyan.pie@gmail.com>
2020-04-19 18:45:22 +0300

    add game Vars

---
+++ core/src/ru/edu/kolyanpie/Vars.java
@@ -0,0 +1,11 @@
+package ru.edu.kolyanpie;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+
+public final class Vars {
+    public static Game game;
+    public static Skin skin;
+
+    private Vars() {}
+}
