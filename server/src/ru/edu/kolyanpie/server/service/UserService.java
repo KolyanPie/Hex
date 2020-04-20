a07c8c1d8456a79c1cd8041939bc6cee8481af24
kolyanpie <k0lyan.pie@gmail.com>
2020-04-21 01:40:01 +0300

    update loadByUsername

--- server/src/ru/edu/kolyanpie/server/service/UserService.java
+++ server/src/ru/edu/kolyanpie/server/service/UserService.java
@@ -1,11 +1,15 @@
 package ru.edu.kolyanpie.server.service;
 
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import ru.edu.kolyanpie.server.model.User;
 import ru.edu.kolyanpie.server.repos.UserRepo;
+
+import java.util.Collections;
+import java.util.List;
 
 @Service
 public class UserService implements UserDetailsService {
@@ -17,12 +21,21 @@ public class UserService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+        User user = userRepo.findByUsername(username);
+
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found");
+        }
+        List<SimpleGrantedAuthority> authorities = Collections.singletonList(new SimpleGrantedAuthority("user"));
+        return new org.springframework.security.core.userdetails.User(
+                user.getUsername(),
+                user.getPassword(),
+                authorities
+        );
     }
 
     public boolean addUser(User user) {
         User userFromDb = userRepo.findByUsername(user.getUsername());
-
         if (userFromDb != null) {
             return false;
         }
