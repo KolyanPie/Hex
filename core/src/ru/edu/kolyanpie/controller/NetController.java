8bc6a0058d55b53c7bb9c29e7d4f1ae2b6e0407b
kolyanpie <k0lyan.pie@gmail.com>
2020-04-22 15:58:00 +0300

    add NetController

---
+++ core/src/ru/edu/kolyanpie/controller/NetController.java
@@ -0,0 +1,28 @@
+package ru.edu.kolyanpie.controller;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Net;
+import com.badlogic.gdx.utils.Json;
+
+public final class NetController {
+    private static final Net.HttpRequest jsonRequest = new Net.HttpRequest(Net.HttpMethods.POST) {{
+        setHeader("Content-Type", "application/json");
+        setHeader("Accept", "application/json");
+    }};
+    private static final Net.HttpRequest getRequest = new Net.HttpRequest(Net.HttpMethods.GET);
+    private static final String url = new Json().fromJson(String.class, Gdx.files.internal("properties/url.json"));
+
+    public static void sendGet(String path, String params, Net.HttpResponseListener listener) {
+        getRequest.setUrl(url + path);
+        getRequest.setContent(params);
+        Gdx.net.sendHttpRequest(getRequest, listener);
+    }
+
+    public static void sendJson(String path, String json, Net.HttpResponseListener listener) {
+        jsonRequest.setUrl(url + path);
+        jsonRequest.setContent(json);
+        Gdx.net.sendHttpRequest(jsonRequest, listener);
+    }
+
+    private NetController() {}
+}
