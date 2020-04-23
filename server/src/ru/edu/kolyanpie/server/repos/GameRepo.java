a7748eacbb6a3b773c0cc20fe320d50dd82e04c7
kolyanpie <k0lyan.pie@gmail.com>
2020-04-23 19:43:46 +0300

    add GameRepo

---
+++ server/src/ru/edu/kolyanpie/server/repos/GameRepo.java
@@ -0,0 +1,7 @@
+package ru.edu.kolyanpie.server.repos;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import ru.edu.kolyanpie.server.model.Game;
+
+public interface GameRepo extends JpaRepository<Game, String> {
+}
